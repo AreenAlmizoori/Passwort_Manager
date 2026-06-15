8c92aaf4e2d146e6d6813dcfbd51da01e9b77645
Areen <areen.almizoori@gmail.com>
2026-06-15 10:50:23 +0200

    generated getter and setter for all attributes of the Entry class

--- src/Entry.java
+++ src/Entry.java
@@ -8,4 +8,28 @@ public class Entry {
         this.username = " ";
         this.password = " ";
     }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
