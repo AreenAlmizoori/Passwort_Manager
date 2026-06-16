6e45622825664caa076a7edeeff9a6a531299205
Areen <areen.almizoori@gmail.com>
2026-06-16 08:28:25 +0200

    deleted unnecessary comments/code

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -178,87 +178,10 @@ public class MainMenu extends Application {
                     view.getChildren().addAll(newLabel, websiteOrApp, username, password, space);
                     labelsCreated++;
                 }
-
-//                int labelsCreated = 0;
-//                int categoryEntries = mainCat.getContents().size();
-//                List<Entry> contents = mainCat.getContents();
-//                while(labelsCreated < categoryEntries){
-//                    Entry entry = contents.get(labelsCreated);
-//                    Label newLabel = new Label("Entry " + (labelsCreated+1));
-//                    Label websiteOrApp = new Label(entry.getApplication());
-//                    Label username = new Label(entry.getUsername());
-//                    Label password = new Label(entry.getPassword());
-//
-//                    view.getChildren().addAll(newLabel, websiteOrApp, username, password);
-//                    labelsCreated++;
-//                }
                 viewStage.show();
             }
         });
 
-//        //BorderPane Elements
-//        //Top Element
-//        HBox topArea = new HBox();
-//        Label programName = new Label("Password Manager.");
-//        topArea.getChildren().add(programName);
-//        mainMenu.setTop(topArea);
-//        topArea.setPrefHeight(30);
-//
-//        //Left Element
-//        AnchorPane anchorPaneLeft = new AnchorPane();
-//        Label categoryOptions = new Label("Category Options:");
-//        Button addCategory = new Button("+");
-//        Button removeCategory = new Button("-");
-//        anchorPaneLeft.getChildren().addAll(addCategory, removeCategory, categoryOptions);
-//        mainMenu.setLeft(anchorPaneLeft);
-//
-//
-//        //Button und Label placement and width (for anchorPaneLeft)
-//        categoryOptions.setLayoutX(0);
-//        categoryOptions.setLayoutY(20);
-//        addCategory.setLayoutX(0);
-//        addCategory.setLayoutY(40);
-//        removeCategory.setLayoutX(40);
-//        removeCategory.setLayoutY(40);
-//        addCategory.setPrefWidth(30);
-//        removeCategory.setPrefWidth(30);
-//
-//        //Button Tooltip (for anchorPaneLeft)
-//        Tooltip addCatTooltip = new Tooltip("Click this to create a new Category");
-//        Tooltip remCatTooltip = new Tooltip("Click this to remove a Category");
-//        addCategory.setTooltip(addCatTooltip);
-//        removeCategory.setTooltip(remCatTooltip);
-//
-//        //Right Element
-//        AnchorPane anchorPaneRight = new AnchorPane();
-//        Label entryOptions = new Label("Entry Options:");
-//        Button addEntry = new Button("+");
-//        Button removeEntry = new Button("-");
-//        anchorPaneRight.getChildren().addAll(addEntry, entryOptions, removeEntry);
-//        mainMenu.setRight(anchorPaneRight);
-//
-//        //Button und Label placement and width (for anchorPaneRight)
-//        entryOptions.setLayoutX(430);
-//        entryOptions.setLayoutY(20);
-//        addEntry.setLayoutX(430);
-//        addEntry.setLayoutY(40);
-//        removeEntry.setLayoutX(430);
-//        removeEntry.setLayoutY(40);
-//        addEntry.setPrefWidth(30);
-//        removeEntry.setPrefWidth(30);
-//
-//        //Button Tooltip (for anchorPaneRight)
-//        Tooltip addEntTooltip = new Tooltip("Click this to create a new Entry");
-//        Tooltip remEntTooltip = new Tooltip("Click this to remove an Entry");
-//        addEntry.setTooltip(addEntTooltip);
-//        removeEntry.setTooltip(remEntTooltip);
-//
-//        //Center Element
-//        VBox categories = new VBox();
-//        Button mainCategory = new Button("Main");
-//        categories.getChildren().add(mainCategory);
-//        mainMenu.setCenter(categories);
-//        categories.setPadding(new Insets(20,10,0,10));
 
 
         stage.setTitle("Password Manager");
