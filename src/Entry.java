ff70d2848c983a21633a393456b2a1df1bf48c9c
Areen <areen.almizoori@gmail.com>
2026-06-21 14:42:36 +0200

    added new id attribute in Entry.java, created getter and setter for this attribute, added equals method and hashcode

--- src/Entry.java
+++ src/Entry.java
@@ -1,20 +1,25 @@
+import java.util.Objects;
+
 public class Entry {
     private String application;
     private String username;
     private String password;
-    private static int entryID;
+    private int id;
+    private static int entryID = 0;
 
     public Entry(){
         this.application = " ";
         this.username = " ";
         this.password = " ";
         entryID++;
+        this.id = entryID;
     }
 
-    public Entry(String application, String username, String password){
+    public Entry(String application, String username, String password, int id){
         this.application = application;
         this.username = username;
         this.password = password;
+        this.id = id;
         entryID++;
     }
 
@@ -50,8 +55,28 @@ public class Entry {
         Entry.entryID = entryID;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Entry entry = (Entry) o;
+        return id == entry.id && Objects.equals(application, entry.application) && Objects.equals(username, entry.username) && Objects.equals(password, entry.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(application, username, password, id);
+    }
+
     @Override
     public String toString() {
-        return "Application/Website: " + application + ", Username " + username + ", Password " + password;
+        return "Application/Website: " + application + ", Username " + username + ", Password " + password + ", ID: " + id;
     }
 }
